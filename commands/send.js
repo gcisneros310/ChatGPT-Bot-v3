c01ad8b5495166b93882d786cc207e11f4cb467e
gcisneros310 <48630000+gcisneros310@users.noreply.github.com>
2023-02-21 11:41:51 -0800

    making minor mods to send.js, removing some unnecessary files

--- commands/send.js
+++ commands/send.js
@@ -13,15 +13,20 @@ module.exports = {
         .setDescription('Send a request to ChatGPT')
         .addStringOption(option => option.setName('prompt').setDescription('prompt you want to send chatgpt').setRequired(true)),       
     async execute(interaction) {
-        const prompt = interaction.options.getString('prompt'); // receive input as string
-        await interaction.deferReply(); // defer reply to fully receive response from API
-        const api = await apiPromise;
-        const response = await api.sendMessage(prompt, {
-            conversationId: conversationId, // 
-            parentMessageId: parentMessageId
-          })
-        conversationId = response.conversationId; // record ID of last conversation for future followups
-        parentMessageId = response.id; // likewise for parent message ID
-        await interaction.editReply(response.text); // send reply back to user after response is fully generated
+        try {
+            const prompt = interaction.options.getString('prompt'); // receive input as string
+            await interaction.deferReply(); // defer reply to fully receive response from API
+            const api = await apiPromise;
+            const response = await api.sendMessage(prompt, {
+                conversationId: conversationId, // 
+                parentMessageId: parentMessageId
+              })
+            conversationId = response.conversationId; // record ID of last conversation for future followups
+            parentMessageId = response.id; // likewise for parent message ID
+            await interaction.editReply(response.text); // send reply back to user after response is fully generated
+        }
+        catch(err) {
+            console.log(err)
+        }
     }
 }
